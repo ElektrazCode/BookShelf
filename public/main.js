5615f36ab71f4598a312e4c1f92f08153ed1a260
ElektrazCode <ElektrazCode@gmail.com>
2022-06-19 18:20:20 -0400

    Fixed Directory Error

---
+++ public/main.js
@@ -0,0 +1,13 @@
+const exploreBtn = document.querySelector(".explore");
+exploreBtn.addEventListener('click', listAllBooks);
+console.log("");
+function listAllBooks(){
+    console.log("Explore Button has been clicked!");
+    fetch('/books',{
+        method: 'get'
+        // headers: {'Content-Type': 'application/json'}
+    })
+    .then(results => {
+        conosle.log(results.json());
+    })
+}
